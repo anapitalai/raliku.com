5666ea640e1a8aed241fa7ef5daf4b0216d43204
Alois Napitalai <anapitalai@gmail.com>
2019-04-25 15:57:21 +1000

    added the property form

--- src/app/crib-form/crib-form.component.ts
+++ src/app/crib-form/crib-form.component.ts
@@ -1,4 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import {CribService} from '@sly/services/crib.service';
+
+@Component({
+  selector: 'app-crib-form',
+  templateUrl: './crib-form.component.html',
+  styleUrls: ['./crib-form.component.css']
+})
+export class CribFormComponent implements OnInit {
+  @ViewChild('newCribForm') newCribForm: NgForm;
+  propertyTypes: Array<string> = ['Condo', 'Duplex', 'House'];
+  
+  constructor(private cribService:CribService) { }
+
+  ngOnInit() {
+  }
+  onCribSubmit(data) {
+    this.cribService.addCrib(data);
+    this.newCribForm.reset();
+  }
+}
+
+/** import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-crib-form',
@@ -12,4 +35,4 @@ export class CribFormComponent implements OnInit {
   ngOnInit() {
   }
 
-}
+}**/
